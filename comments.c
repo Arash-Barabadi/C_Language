40be2935bbb8a14d363caa1129bd866d719a819d
Arash <arash.barabadi@gmail.com>
2024-02-09 12:03:07 +0100

    The comment.c file is generated.

---
+++ comments.c
@@ -0,0 +1,17 @@
+// The following line is a header file, which contains printf() function.
+#include <stdio.h> 
+
+
+int main (){
+    /*int in the following command used to declare the variable "age". then the value 42 is assigned to the age.
+    and I ended the statement with ; as it's the RULE. o*/
+    //storing the age
+    int age = 42; //some demo value
+
+    /*The printf command has been used to output the variable 'age'. But before that it's necessary to specify
+    the format of the 'age' variable. The format specifier here is "%d", which is used for integer data type*/
+    
+    printf("%d\n", age);
+
+    return 0;
+}
