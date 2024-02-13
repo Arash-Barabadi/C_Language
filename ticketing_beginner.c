f8589c5a58a4fa6fc912cd4acb116e63e400428d
Arash <arash.barabadi@gmail.com>
2024-02-14 00:00:02 +0100

    simple ticketing .c file.

---
+++ ticketing_beginner.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+    int tickets = 73;
+    float price = 7.45;
+
+    float total_price = tickets*price;
+    printf("Total: %f", total_price);
+
+    return 0;
+}
