e39e00ef47dc3a4de9d054b96fc89dff08ba123a
Barabadi <uig39818@contiwan.com>
2024-02-22 14:22:25 +0100

    The new folder for the function section created.

---
+++ Functions/01_Functions_Basics.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+void greeting() {
+    printf("Hello! \n");
+    printf("I am an example function.");
+}
+
+// void : means that this function does not have a return value. 
+
+
+/**
+ * If you want to call a function use it followed parentheses. 
+*/
+int main() {
+    greeting() ; 
+
+    return 0;
+}
