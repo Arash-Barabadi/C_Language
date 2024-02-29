79c8f206c51196924b9ee68397f55626d38f1d49
Barabadi <uig39818@contiwan.com>
2024-02-29 15:46:51 +0100

    The pointer definition and it's use in swapping value.

---
+++ Pointers/pointers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * Passing pointers to functions is a great way to solve some challenging tasks. 
+ * One example is swapping value. 
+ * You have two variable values, which are taken from input, and you need to swap the values. 
+ * The given code declares two variables, takes them from input and passes them to a swap() function, as pointers. 
+ * The swap() function needs to swap their values.
+ * Task: Define the swap() function so that given code works as expected. 
+*/
